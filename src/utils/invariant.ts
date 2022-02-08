de736cb34d475cc6c88e455b131b0476f474dff2
Arjun <14841132+arjunvegda@users.noreply.github.com>
2022-02-07 20:11:10 -0500

    style: fix typo + remove comments

--- src/utils/invariant.ts
+++ src/utils/invariant.ts
@@ -1,15 +1,7 @@
-// const defaultMessage = 'Invariant failed, an error occurred in " library';
-
-// Throw an error if the condition fails
-// Strip out error messages for production
-// > Not providing an inline default argument for message as the result is smaller
-
 import { makeString } from './make-string';
 
 export function invariant(
   condition: any, // eslint-disable-line @typescript-eslint/no-explicit-any
-  // Can provide a string, or a function that returns a string for cases where
-  // the message takes a fair amount of effort to compute
   message: string,
 ): asserts condition {
   if (condition) {
