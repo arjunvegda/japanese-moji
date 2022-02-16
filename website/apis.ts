2e6e981453b2e967068adf123e72624620f09a96
Arjun <14841132+arjunvegda@users.noreply.github.com>
2022-02-15 20:59:48 -0500

    docs(website): improve performance

---
+++ website/apis.ts
@@ -0,0 +1,65 @@
+interface FetchVersionReturn {
+  releaseVersion: string | null;
+}
+
+const fetchVersion = async (): Promise<FetchVersionReturn> => {
+  const result = await fetch('https://cdn.jsdelivr.net/npm/japanese-moji/package.json')
+    .then((res) => res.json())
+    .catch(() => {
+      return { version: null };
+    });
+
+  return {
+    releaseVersion: result?.version,
+  };
+};
+
+interface FetchBundleSizeReturn {
+  gzipBundleSize: number | null;
+}
+
+const fetchBundleSize = async (): Promise<FetchBundleSizeReturn> => {
+  const result = await fetch('https://bundlephobia.com/api/size?package=japanese-moji')
+    .then((res) => res.json())
+    .catch(() => {
+      return { gzip: null };
+    });
+
+  return {
+    gzipBundleSize: result?.gzip,
+  };
+};
+
+interface FetchCodeCovReturn {
+  codeCoverage: number;
+}
+const fetchCodeCov = async (): Promise<FetchCodeCovReturn> => {
+  const result = await fetch('https://codecov.io/api/gh/arjunvegda/japanese-moji/branch/main/')
+    .then((res) => res.json())
+    .catch(() => {
+      return {};
+    });
+
+  const coverage = result?.commit?.totals ? result.commit.totals?.c : 0;
+  return {
+    codeCoverage: coverage,
+  };
+};
+
+interface FetchPackageInfoReturn
+  extends FetchVersionReturn,
+    FetchBundleSizeReturn,
+    FetchCodeCovReturn {}
+
+export const fetchPackageInfo = async (): Promise<FetchPackageInfoReturn> => {
+  const response = await Promise.all([fetchVersion(), fetchBundleSize(), fetchCodeCov()]);
+  const responseToObject = response.reduce<FetchPackageInfoReturn>(
+    (acc, curr) => ({
+      ...acc,
+      ...curr,
+    }),
+    {} as FetchPackageInfoReturn,
+  );
+
+  return responseToObject;
+};
